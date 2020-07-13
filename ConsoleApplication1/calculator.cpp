405a03a551146df463da3985253d4550bcd127ab
Kidwell <akidwell@GAIG.COM>
2020-07-13 14:30:29 -0400

    Add project files.

---
+++ ConsoleApplication1/calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+double calculator::Calculate(double x, char oper, double y)
+{
+
+	switch (oper)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+
+	default:
+		return 0.0;
+	}
+}
---
+++ ConsoleApplication1/calculator.h
@@ -0,0 +1,10 @@
+#pragma once
+class calculator
+{
+public:
+	double Calculate(double x, char oper, double y);
+
+
+
+};
+
